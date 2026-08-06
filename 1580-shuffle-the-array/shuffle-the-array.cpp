5c7c0bdbe226bfdd49bbafb35f6b5dfb070c3f0a
kashakkhan <kashakkhantomar@gmail.com>
2026-08-06 23:30:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (53.67%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -6,16 +6,11 @@ public:
         vector<int> c;
         for (int i=0 ;i<n;i++){
             a.push_back(nums[i]);
-        }
-        for(int i=n;i<nums.size();i++){
-            b.push_back(nums[i]);
-        }
-        for (int i =0;i<n;i++){
-            c.push_back(a[i]);
-            c.push_back(b[i]);
-
-        }
-        return c ;
+            a.push_back(nums[i+n]);
+            }
+        
+        
+        return a ;
         
         
     }
