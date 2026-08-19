5469833d83fbc94a9ec901a4bab830b9540296ce
kashakkhan <kashakkhantomar@gmail.com>
2026-08-19 23:27:27 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (36.75%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if (find(nums.begin(),nums.end(),target)!=nums.end()){
+            return (find(nums.begin(),nums.end(),target)-nums.begin()) ;
+        }
+        else {
+            return -1;
+        }
+        
+    }
+};
